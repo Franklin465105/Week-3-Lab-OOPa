bcf4e2384ae16e1fd8dde686c56bd21c170388a1
Franklin Esedebe <G00465105@atu.ie>
2025-10-01 17:22:02 +0100

    Added new method called add

---
+++ src/main/java/ie/atu/week3/Calculator.java
@@ -0,0 +1,12 @@
+package ie.atu.week3;
+
+public class Calculator {
+    public Calculator() {
+    }
+
+    public int add(int firstNum, int secondNum)
+    {
+        return firstNum + secondNum;
+    }
+    // Subtract/Divide/Multiply later
+}
